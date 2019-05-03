78115cc7be703372f3570588217ffe7fb6b70cb0
Shisei Hanai <ruimo.uno@gmail.com>
2019-05-03 20:17:55 +0900

    Fix connection.

--- src/main/scala/com/ruimo/mpc4s/Connection.scala
+++ src/main/scala/com/ruimo/mpc4s/Connection.scala
@@ -6,7 +6,10 @@ import java.net.Socket
 trait Connection {
   def clearError(): Unit
   def stop(): Unit
+  def clear(): Unit
   def lsInfo(path: Option[String]): Response.LsInfo
+  def add(path: String): Unit
+  def play(idx: Option[Int]): Unit
   def status(): Response.StatusInfo
   def currentSong(): Option[Response.SongInfo]
 }
